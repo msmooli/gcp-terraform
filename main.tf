0fb0504c7701f99ea69cb378a18b9ce8a300fc12
msmooli <markmooli187@gmail.com>
2023-11-04 15:26:13 -0400

    initial code

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "google_storage_bucket" "my-bucket" {
   name                     = "open-get-bucket"
   project                  = "terraform-proj-403900"
   location                 = "US"
-  force_destroy            =  true
+  force_destroy            = true
   public_access_prevention = "enforced"
 }
